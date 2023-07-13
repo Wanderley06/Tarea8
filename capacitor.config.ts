1e0d3f90441f3cb0ad6831cfc3fe4f4c2a112a5e
Wanderley06 <95730075+Wanderley06@users.noreply.github.com>
2023-07-12 23:54:12 -0400

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'aplic.vivencias',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
